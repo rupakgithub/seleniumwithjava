6dd5f4389b3326af2acd4fd9a8a00457b8ac2c68
Rupak <rbhattacharjee26@yahoo.com>
2023-06-24 08:00:13 +0530

    Updated driver dependency and code

--- src/test/java/org/example/SeleniumTEsts.java
+++ src/test/java/org/example/SeleniumTEsts.java
@@ -1,36 +1,36 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class SeleniumTEsts {
 
-    WebDriver driver = null;
-    String chromedriverpath = "C:\\Users\\ASUS\\Desktop\\git example\\seleniumwithjava\\src\\test\\drivers\\chromedriver.exe";
+    WebDriver driver;
 
     @BeforeTest
-    public void beforetest(){
-        System.out.println("This is before test");
+    public void beforetest() {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver",chromedriverpath);
-        driver.get("https://www.saucedemo.com/");
-
     }
 
     @Test(priority = 1)
-    public void firsttest(){
-      String title = driver.getTitle();
-      if(title.equals("Swag Labs")){
-          Assert.assertTrue(true);
-      }else {
-          Assert.fail();
-      }
+    public void firsttest() {
+        driver.get("https://www.saucedemo.com/");
+        String title = driver.getTitle();
+        if (title.equals("Swag Labs")) {
+            Assert.assertTrue(true);
+        } else {
+            Assert.fail();
+        }
     }
 
     @AfterTest
-    public void aftertest(){
+    public void aftertest() {
         driver.quit();
     }
 
