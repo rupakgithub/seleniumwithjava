2c1252635479b4fa7e354adb405aeaccba37b79b
Rupak <rbhattacharjee26@yahoo.com>
2023-06-27 08:46:48 +0530

    Added ADD TO CART scenario

--- src/test/java/org/example/SeleniumTEsts.java
+++ src/test/java/org/example/SeleniumTEsts.java
@@ -3,6 +3,7 @@ package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -19,6 +20,7 @@ public class SeleniumTEsts {
         WebDriverManager.chromedriver().setup();
         ChromeOptions ops = new ChromeOptions();
         ops.addArguments("--remote-allow-origins=*");
+        ops.addArguments("--start-maximized");
         driver = new ChromeDriver(ops);
 
     }
@@ -36,7 +38,7 @@ public class SeleniumTEsts {
         }
     }
 
-    @Test
+    @Test(enabled = false)
     public void positive_login_scenario() throws InterruptedException {
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.cssSelector("input[id='user-name']")).sendKeys("locked_out_user");
@@ -51,6 +53,30 @@ public class SeleniumTEsts {
             Assert.fail("Text doesn't contains locked out user");
         }
         Thread.sleep(3000);
+    }
+
+    @Test
+    public void cart_scenario() throws InterruptedException {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.cssSelector("input[id='user-name']")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("input[id='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("input[id='login-button']")).click();
+        Thread.sleep(1000);
+
+        for (int i=1; i <= 2; i++){
+            driver.findElement(By.xpath("(//button[text()='Add to cart'])["+i+"]")).click();
+            Thread.sleep(1000);
+        }
+
+        String cartItems = driver.findElement(By.xpath("//div[@id='shopping_cart_container']//span")).getText();
+
+        if (Integer.parseInt(cartItems) == 2){
+            Assert.assertTrue(true);
+        }
+        else {
+            Assert.fail("Cart don't have exactly 2 items");
+        }
+        Thread.sleep(2000);
     }
 
     @AfterTest
