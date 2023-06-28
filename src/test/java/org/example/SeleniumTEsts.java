155ebf99ae80da3bc1009b62de3ad8664824c25e
Rupak <rbhattacharjee26@yahoo.com>
2023-06-28 08:11:29 +0530

    Add amount validation test case

--- src/test/java/org/example/SeleniumTEsts.java
+++ src/test/java/org/example/SeleniumTEsts.java
@@ -11,6 +11,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SeleniumTEsts {
@@ -83,7 +85,7 @@ public class SeleniumTEsts {
     }
 
     @Test(priority = 3)
-    public void verify_total_items(){
+    public void verify_total_items() throws InterruptedException {
         driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
        List<WebElement> element = driver.findElements(By.xpath("//div[@class='cart_list']/div[@class='cart_item']"));
        if(element.size() == 2){
@@ -91,6 +93,37 @@ public class SeleniumTEsts {
        }else {
            Assert.fail("Cart don't have exactly 2 items");
        }
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 4)
+    public void verify_total_amount() throws InterruptedException {
+        driver.findElement(By.xpath("//button[@id='checkout']")).click();
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//input[@id='first-name']")).sendKeys("Rupak");
+        driver.findElement(By.xpath("//input[@id='last-name']")).sendKeys("Bha");
+        driver.findElement(By.xpath("//input[@id='postal-code']")).sendKeys("784562");
+        driver.findElement(By.xpath("//input[@id='continue']")).click();
+        Thread.sleep(500);
+
+        List<Double> inventory_price = new ArrayList<>();
+
+        List<WebElement> element = driver.findElements(By.xpath("//div[@class='inventory_item_price']"));
+
+        for(int i = 0; i < element.size(); i++) {
+            String price = element.get(i).getText().replaceAll("[a-zA-Z$]","").trim();
+            System.out.println(price);
+            inventory_price.add(Double.parseDouble(price));
+        }
+
+        String final_price = driver.findElement(By.xpath("//div[contains(@class,'summary_total_label')]")).getText().replaceAll("[a-zA-Z$:]","").trim();
+        inventory_price.add(Double.parseDouble(final_price));
+
+        if((inventory_price.get(0)+inventory_price.get(1)) <= inventory_price.get(2)){
+            Assert.assertTrue(true);
+        }else {
+            Assert.fail("Sum total is lesser than sum of individual prices");
+        }
     }
 
     @AfterTest
