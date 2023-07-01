6973e94b081f619bea8021dffae26b6a7b8b19f1
Rupak <rbhattacharjee26@yahoo.com>
2023-07-01 08:33:02 +0530

    Added download & color tests

--- src/test/java/org/example/WidgetsTest.java
+++ src/test/java/org/example/WidgetsTest.java
@@ -16,6 +16,7 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 public class WidgetsTest {
@@ -25,11 +26,15 @@ public class WidgetsTest {
     @BeforeTest
     public void beforetest() {
         WebDriverManager.chromedriver().setup();
+        String downloadFilepath = "C:\\Users\\ASUS\\Desktop\\git example\\seleniumwithjava\\downloads";
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", downloadFilepath);
         ChromeOptions ops = new ChromeOptions();
         ops.addArguments("--remote-allow-origins=*");
         ops.addArguments("--start-maximized");
         ops.addArguments("--incognito");
-        ops.addArguments("--disable-geolocation");
+        ops.setExperimentalOption("prefs",chromePrefs);
         ops.setExperimentalOption("useAutomationExtension", false);
         ops.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
         driver = new ChromeDriver(ops);
@@ -70,6 +75,40 @@ public class WidgetsTest {
         actions.dragAndDrop(drag, drop).perform();
 
         Thread.sleep(5000);
+    }
+
+    @Test
+    public void test_button_color() throws InterruptedException {
+        driver.get("https://demoqa.com/dynamic-properties");
+        WebElement button = driver.findElement(By.xpath("//button[@id='colorChange']"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+
+        String colorbefore = button.getCssValue("color");
+        System.out.println(colorbefore);
+
+        button.click();
+
+        Thread.sleep(3000);
+
+        String colorafter = button.getCssValue("color");
+        System.out.println(colorafter);
+
+        assert colorbefore != colorafter;
+    }
+
+    @Test
+    public void test_download() throws InterruptedException {
+        driver.get("https://demoqa.com/upload-download");
+
+        WebElement button = driver.findElement(By.xpath("//a[@id='downloadButton']"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+
+        button.click();
+
+        Thread.sleep(5000);
+
     }
 
 
