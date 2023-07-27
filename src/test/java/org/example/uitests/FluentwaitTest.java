721937ae5765076b662aafc67c910573d3d3ec6e
Rupak <rbhattacharjee26@yahoo.com>
2023-07-27 14:05:05 +0530

    Added fix for fluentwait

--- src/test/java/org/example/uitests/FluentwaitTest.java
+++ src/test/java/org/example/uitests/FluentwaitTest.java
@@ -51,7 +51,7 @@ public class FluentwaitTest {
         driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        Wait<WebDriver> fluentwait = new FluentWait<WebDriver>(driver)
+        Wait<WebDriver> fluentwait = new FluentWait<>(driver)
                 .withTimeout(Duration.ofSeconds(5))
                 .pollingEvery(Duration.ofMillis(100))
                 .ignoring(NoSuchElementException.class);
@@ -63,21 +63,18 @@ public class FluentwaitTest {
         WebElement saveButton = driver.findElement(By.xpath("(//button[@type='submit'])[1]"));
         wait.until(ExpectedConditions.visibilityOf(saveButton));
         Thread.sleep(3000);
+        By message = By.xpath("//div[@id='oxd-toaster_1']//p[contains(@class,'toast-message')]");
         saveButton.click();
 
-        //By message = By.xpath("//*[contains(text(),'Successfully')]");
-
-        //fluentwait.until(ExpectedConditions.presenceOfElementLocated(message));
+        fluentwait.until(ExpectedConditions.visibilityOfElementLocated(message));
+        String messagetext = driver.findElement(message).getText();
         System.out.println("Element located");
         Thread.sleep(1000);
-        if (driver.getPageSource().contains("Successfully")){
+        if (messagetext.contains("Successfully Updated")){
             Assert.assertTrue(true);
         } else {
             Assert.fail("Success message not present");
         }
-
-
-
 
     }
 
