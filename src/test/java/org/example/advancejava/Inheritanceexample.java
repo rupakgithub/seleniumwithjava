7a5ad9edc2f8117717e8704de33338945700faea
Rupak <rbhattacharjee26@yahoo.com>
2023-09-21 08:49:39 +0530

    Added super and other inheritance concept

--- src/test/java/org/example/advancejava/Inheritanceexample.java
+++ src/test/java/org/example/advancejava/Inheritanceexample.java
@@ -2,20 +2,34 @@ package org.example.advancejava;
 
 public class Inheritanceexample {
     public static void main(String[] args) {
-        Parrot parrot = new Parrot();
-        parrot.wing();
-        parrot.color();
+        Color color = new Color();
+        color.wing();
+        color.color();
+        //color.shade();
     }
 }
 
 class Bird{
-    void wing(){
+    protected void wing(){
         System.out.println("Have 2 wings");
     }
 }
 
 class Parrot extends Bird{
+
     void color(){
         System.out.println("have green color");
+    }
+}
+
+class Color extends Parrot {
+
+    void color(){
+        super.color();
+        System.out.println("have green color from color class");
+    }
+
+    void shade() {
+        System.out.println("shade name is light green ");
     }
 }
